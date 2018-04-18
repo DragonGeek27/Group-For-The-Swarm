924fd17d1d544d006fc191b28623c1c59adad7d6
DragonGeek27 <dragongeek27@gmail.com>
2018-04-18 12:49:59 -0600

    update
    update

--- Buzzzy as a Bee/Assets/Target.cs
+++ Buzzzy as a Bee/Assets/Target.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
 
-    private float RotateSpeed = 5f;
-    private float Radius = 0.1f;
-
-    private Vector2 _centre;
-    private float _angle;
-
     float timeCounter = 0;
     float speed = 10;
     public Transform target;
@@ -19,7 +13,6 @@ public class Target : MonoBehaviour
 
     private void Start()
     {
-        _centre = transform.position;
     }
 
     void Update()
