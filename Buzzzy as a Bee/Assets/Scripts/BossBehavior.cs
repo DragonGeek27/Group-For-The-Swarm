a644f6e9492bdf3f96c8478918f5d27ec92ac8b2
DragonGeek27 <dragongeek27@gmail.com>
2018-04-29 21:06:01 -0600

    final
    final

---
+++ Buzzzy as a Bee/Assets/Scripts/BossBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BossBehavior : MonoBehaviour {
+    public Transform target;
+    public float forwardSpeed;
+    public GameObject explosion;
+
+    public float rightLimit = 2.5f;
+    public float leftLimit = 1.0f;
+    public float speed = 2.0f;
+    private int direction = 1;
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+                                // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+    public bool isHere = false;
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+    public GameObject collide;
+    private Vector3 startPos;
+    public Image healthBar;
+    public int bossHealth;
+    private float birdHealth = 10;
+    private static int maxHealth = 50;
+    private static int pleaseWork = 50;
+    public void Start()
+    {
+        bossHealth = 100;
+        startPos = transform.position;
+        birdHealth = 10;
+    }
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        Vector3 target = new Vector3(0, 0, 13);
+        transform.rotation = Quaternion.Euler(0, -180, 0);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Bolt")
+        {
+            pleaseWork--;
+            healthBar.fillAmount = (float)pleaseWork / (float)maxHealth;
+            Instantiate(explosion, transform.position, transform.rotation);
+            Debug.Log(pleaseWork);
+            if (pleaseWork == 0)
+            {
+                birdHealth = bossHealth;
+                Destroy(this.gameObject);
+                Debug.Log(birdHealth + ", " + bossHealth);
+            }
+        }
+    }
+    public bool isDestroyed()
+    {
+        if (pleaseWork == 0)
+        {
+            Debug.Log("PLEASE!");
+            pleaseWork = 100;
+            return true;
+        }
+        else
+        {
+            Debug.Log(pleaseWork);
+            return false;
+        }
+    }
+}
--- Buzzzy as a Bee/Assets/Scripts/GameController.cs
+++ Buzzzy as a Bee/Assets/Scripts/GameController.cs
@@ -1,47 +1,68 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : MonoBehaviour
 {
     public GameObject[] hazards;
+    public GameObject signInMenu;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
+    public GameObject bossPrefab;
 
-    public GUIText scoreText;
+    public Text scoreText;
     public GUIText restartText;
     public GUIText gameOverText;
     public GUIText mainMenuText;
+    private BossBehavior script;
 
     private bool gameOver;
     private bool restart;
     private bool mainMenu;
     private int score;
+    public int _score;
+    private bool bossBattle;
+    private int bossCount;
 
     void Start()
     {
         gameOver = false;
+        bossCount = 1;
         restart = false;
         mainMenu = false;
         restartText.text = "";
         gameOverText.text = "";
         mainMenuText.text = "";
+        signInMenu.SetActive(false);
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnWaves());
+        script = bossPrefab.GetComponent<BossBehavior>();
     }
 
     void Update()
     {
+        //if (restart)
+        //{
+        //    Restart();
+        //}
 
+        if (script.isDestroyed())
+        {
+            bossCount++;
+            score += 250;
+            Debug.Log("What's the deal?");
+            StartCoroutine(SpawnWaves());
+        }
     }
 
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
-        while (true)
+        while (score / (1000 * bossCount) != 1)
         {
             for (int i = 0; i < hazardCount; i++)
             {
@@ -50,18 +71,23 @@ public class GameController : MonoBehaviour
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
+                bossBattle = false;
             }
             yield return new WaitForSeconds(waveWait);
-
-            if (gameOver)
-            {
-                restartText.text = "Try Aagain";
-                restart = true;
-                mainMenuText.text = "Start Screen";
-                mainMenu = true;
-                break;
-            }
         }
+        if (score / (1000 * bossCount) == 1)
+        {
+            bossBattle = true;
+            spawnBoss();
+        }
+    }
+
+    public void spawnBoss()
+    {
+        GameObject go = bossPrefab;
+        Vector3 boss = new Vector3(0, 0, 30);
+        Instantiate(go, boss, transform.rotation);
+
     }
 
     public void AddScore(int newScoreValue)
@@ -72,6 +98,7 @@ public class GameController : MonoBehaviour
 
     void UpdateScore()
     {
+        _score = score;
         scoreText.text = "Pollen Count: " + score;
     }
 
@@ -79,5 +106,18 @@ public class GameController : MonoBehaviour
     {
         gameOverText.text = "Game Over!";
         gameOver = true;
+        restartText.text = "Try Again";
+        restart = true;
+        mainMenuText.text = "Start Screen";
+        mainMenu = true;
+        signInMenu.SetActive(true);
     }
+
+    //public void Restart()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Return))
+    //    {
+    //        Application.LoadLevel(Application.loadedLevel);
+    //    }
+    //}
 }
